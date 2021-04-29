e3c6ec6756ad58d881e4c850157a780e04322bda
fernandofossi <fernandofossi@gmail.com>
2021-04-29 20:04:07 -0300

    commit inicial

---
+++ ClassePrincipal.java
@@ -0,0 +1,10 @@
+public class ClassePrincipal{
+    
+    private int atributo1;
+    private int atributo2;
+    
+    public ClassePrincipal(int arg1, int arg2){
+        atributo1 = arg1;
+        atributo2 = arg2;
+    }
+}
---
+++ ClasseSecundaria.java
@@ -0,0 +1,10 @@
+public class ClasseSecundaria{
+    
+    private int atributo1;
+    private int atributo2;
+    
+    public ClasseSecundaria(int arg1, int arg2){
+        atributo1 = arg1;
+        atributo2 = arg2;
+    }
+}
