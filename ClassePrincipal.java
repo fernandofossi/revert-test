309f7056e6f27fdda8c4eda1c82e6936099e551e
Alexssandra Lima <alexssandralimap@gmail.com>
2021-04-29 23:48:34 -0300

    Alteração 1 da release 3.1

--- ClassePrincipal.java
+++ ClassePrincipal.java
@@ -1,11 +1,12 @@
 public class ClassePrincipal{
     
-    private static final int version = 3.0;
+    private static final int version = 3.1;
     
     private static final String author = "fossi";
     
     private int atributo1;
     private int atributo2;
+    private int atributo3;
     
     public ClassePrincipal(int arg1, int arg2){
         atributo1 = arg1;
